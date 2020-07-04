a572f1abb85ce388fb70270f21938087e6cdb124
鈴木寛之 <suzukihiroyuki@suzukihiroyukinoMacBook-Pro.local>
2020-07-04 17:10:33 +0900

    chore: touch sample ant component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Moment } from "moment";
+import "./App.css";
+import "antd/dist/antd.css";
 
-function App() {
+import { DatePicker, message } from "antd";
+
+function App(): React.ReactElement {
+  const [date, setDate] = useState<Moment>();
+  const handleChange = (value: Moment | null): void => {
+    message.info(
+      `Selected Date: ${value ? value.format("YYYY-MM-DD") : "None"}`
+    );
+    setDate(value || undefined);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: 400, margin: "100px auto" }}>
+      <DatePicker onChange={handleChange} />
+      <div style={{ marginTop: 16 }}>
+        Selected Date: {date ? date.format("YYYY-MM-DD") : "None"}
+      </div>
     </div>
   );
 }
